fbbd2711814d5d16ee294ad815261a5b50a5b4e7
leezhengjing <zhengjing.lee86@gmail.com>
2023-01-14 20:48:52 +0800

    starting database

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, URL
 from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor, CKEditorField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
 Bootstrap(app)
-app.secret_key = "some secret string"
+
+##CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 
 class LoginForm(FlaskForm):
     email = StringField(label='email', validators=[DataRequired(), Email(), Length(min=8, message="Email is not long enough!")])
     password = PasswordField(label='password', validators=[DataRequired()])
     submit = SubmitField(label="Log In")
 
-class RegisterForm(FlaskForm):
-    email = StringField(label='email', validators=[DataRequired(), Email(), Length(min=8, message="Email is not long enough!")])
-    password = PasswordField(label='password', validators=[DataRequired()])
-    submit = SubmitField(label="Log In")
+class ProductPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+    price = db.Integer(db.Integer, nullable=False)
 
 @app.route("/")
 def index():
@@ -31,32 +45,13 @@ def details():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-
-    test_email = "admin@email.com"
-    test_pass = "12345678"
-
-    # Validate data
     if login_form.validate_on_submit():
-        if login_form.email.data != test_email or login_form.password.data != test_pass:
-            return render_template("login.html", form=login_form)
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
         else:
-            return render_template("index.html")
+            return render_template("denied.html")
     else:
         return render_template("login.html", form=login_form)
-
-# Work in progress
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    register_form = RegisterForm()
-    login_form = LoginForm()
-
-    if register_form.validate_on_submit():
-        email = register_form.email.data
-        password = register_form.password.data
-
-        return render_template("login.html", form=login_form)
-    else:
-        return render_template("register.html", form=register_form)
 
 if __name__ == "__main__":
     app.run(debug=True)

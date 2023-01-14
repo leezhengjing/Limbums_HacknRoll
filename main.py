64ea1e40263a7a99ec991726e4191eebf8b60555
kurtyjlee <kurtyjlee@gmail.com>
2023-01-14 18:01:04 +0800

    remove header.html

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/details")
+def details():
+
+    # if request.method == "POST":
+    #     job_type = request.form.get("job_type")
+    #     requirement = request.form.get("requirement")
+    #     job_sector = request.form.get("job_sector")
+    #     pay = request.form.get("pay")
+    #     desc = request.form.get("desc")
+    #
+    #     return redirect ("/")
+    # else:
+    return render_template("details.html")
 
 
 if __name__ == "__main__":

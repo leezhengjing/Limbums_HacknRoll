dec8bb5a8b1a3c49135f627200cedfa8515a959e
kurtyjlee <kurtyjlee@gmail.com>
2023-01-14 23:07:40 +0800

    import

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
 from flask import Flask, render_template, send_from_directory, url_for
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, URL
-# from flask_bootstrap import Bootstrap
-# from flask_ckeditor import CKEditor, CKEditorField
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor, CKEditorField
 from flask_uploads import UploadSet, IMAGES, configure_uploads
 
-##CONNECT TO DB
+#CONNECT TO DB
 db = SQLAlchemy()
 # create the app
 app = Flask(__name__)
+
 Bootstrap(app)
 # configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///products.db"
 # initialize the app with the extension
 db.init_app(app)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-# ckeditor = CKEditor(app)
-# Bootstrap(app)
 
 # Dealing with photos
 app.config['UPLOADED_PHOTOS_DEST'] = 'uploads'
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
-
-# ##CONNECT TO DB
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
 
 class LoginForm(FlaskForm):
     email = StringField(label='email', validators=[DataRequired(), Email(), Length(min=8, message="Email is not long enough!")])
@@ -45,15 +39,6 @@ class UploadForm(FlaskForm):
     )
     submit = SubmitField("Upload")
 
-# class ProductPost(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(250), unique=True, nullable=False)
-#     subtitle = db.Column(db.String(250), nullable=False)
-#     date = db.Column(db.String(250), nullable=False)
-#     body = db.Column(db.Text, nullable=False)
-#     author = db.Column(db.String(250), nullable=False)
-#     img_url = db.Column(db.String(250), nullable=False)
-#     price = db.Integer(db.Integer, nullable=False)
 class Products(db.Model):
     __tablename__ = "products"
     id = db.Column(db.Integer, primary_key=True)
@@ -69,7 +54,6 @@ class Products(db.Model):
 with app.app_context():
     db.create_all()
 
-
 class LoginForm(FlaskForm):
     email = StringField(label='email',
                         validators=[DataRequired(), Email(), Length(min=8, message="Email is not long enough!")])
@@ -81,7 +65,6 @@ class LoginForm(FlaskForm):
 def index():
     products = db.session.execute(db.select(Products).order_by(Products.title)).scalars()
     return render_template("index.html", products=products)
-
 
 @app.route("/details")
 def details():
